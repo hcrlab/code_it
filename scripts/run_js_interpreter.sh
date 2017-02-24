835473145159d17c080df84e46343e2b3bf783e4
Justin Huang <jstn@cs.washington.edu>
2017-02-23 18:44:48 -0800

    Added nvm line to run_js_interpreter.

--- scripts/run_js_interpreter.sh
+++ scripts/run_js_interpreter.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+export NVM_DIR="/home/rws/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm use 0.10.40
 cd ../build/bundle
 export ROOT_URL='http://localhost'
 export PORT=3003
+node -v
 node main.js
